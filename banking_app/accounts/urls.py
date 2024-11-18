34bb982b6f069cd72967f251d04329d4be6ac299
Abhik Salian <21e03.abhik@sjec.ac.in>
2024-11-18 21:36:41 +0530

    templates and urls created

---
+++ banking_app/accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('<int:account_id>/summary/', views.account_summary, name='account_summary'),
+    path('<int:account_id>/transfer/', views.transfer_funds, name='transfer_funds'),
+    path('<int:account_id>/pay_bill/', views.pay_bill, name='pay_bill'),
+    path('<int:account_id>/transaction_history/', views.transaction_history, name='transaction_history'),
+]
